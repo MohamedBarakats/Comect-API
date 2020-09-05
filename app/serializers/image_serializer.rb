483ec75b099a32d309fe8b1bab91272dfaf3de93
Mohamed Barakat <MohamedBarakatse@gmail.com>
2020-09-05 20:05:43 +0200

    add image url

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -2,9 +2,15 @@
 
 class ImageSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :description, :tag, :image_url
+  attributes :id, :description, :tag, :image_url, :resized_image_url
 
   def image_url
+    rails_blob_path(object.file, only_path: true) if object.file.attached?
+  end
+
+  def resized_image_url
+    return unless object.file.attached?
+
     Rails.application.routes.url_helpers.rails_representation_url(
       object.file.variant(resize_to_limit: [200, 200]).processed, only_path: true
     )
